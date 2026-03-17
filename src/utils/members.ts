8c42cd05ae3c852bbd0317d2c2c4b2407dfbb3be
Eduardo Yañez Mejía <eduym@192.168.0.225>
2026-03-17 14:21:47 -0600

    adds members images

---
+++ src/utils/members.ts
@@ -0,0 +1,8 @@
+import abelHZ from "../assets/members/abelHZ.jpeg";
+import eduYM from "../assets/members/eduYM.png";
+import newMember from "../assets/members/newMember.png";
+
+
+export {
+  abelHZ, eduYM, newMember
+}
