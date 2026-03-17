ead6ac4e1c9625303307205ebf9c6bea4cb04d7b
Eduardo Yañez Mejía <eduym@192.168.0.225>
2026-03-17 11:28:40 -0600

    adds pages images

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,7 +1,9 @@
 import asteroiDevImagotype from "../assets/asteroidevImagotype.svg";
 import asteroiDevIsotype from "../assets/asteroidevIsotype.svg";
 import heroBg from "../assets/heroBg.svg";
+import alienFriendly from "../assets/alienFriendly.svg";
+import alienTwins from "../assets/alienTwins.png";
 
 export {
-  asteroiDevImagotype, asteroiDevIsotype, heroBg
+  asteroiDevImagotype, asteroiDevIsotype, heroBg, alienFriendly, alienTwins
 }
