582e0016bb0932921fed592a3e2109d659f16e68
Eduardo Yañez Mejía <eduardo_yanez@live.com.mx>
2026-03-23 18:44:47 -0600

    fixes member image

--- src/utils/members.ts
+++ src/utils/members.ts
@@ -1,6 +1,6 @@
 import abelHZ from "../assets/members/abelHZ.jpeg";
 import eduYM from "../assets/members/eduYM.png";
-import julioYM from "../assets/members/eduYM.png";
+import julioYM from "../assets/members/julioYM.jpeg";
 import newMember from "../assets/members/newMember.png";
 
 
